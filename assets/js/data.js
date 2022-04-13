bcac83eba7ea0e4a5aba932f7bdca87e0fd9dfa3
Danielle <daniellekurek@gmail.com>
2022-04-12 21:04:37 -0500

    CSS File Version

--- assets/js/data.js
+++ assets/js/data.js
@@ -6,29 +6,33 @@ var totalProfitMonth = [	91.86	];
 
 // MONTHLY NUMBERS
 var month = ["	January	","	February	","	March	","	April	","	May	","	June	","	July	","	August	","	September	","	October	","	November	","	December	"];
-																								
-var monthlySales = [	185	,	175	,	234	,	22	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
-var ebayMonthlySales = [	98	,	106	,	144	,	13	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
-var poshMonthlySales = [	41	,	58	,	72	,	5	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
-var mercariMonthlySales = [	21	,	5	,	16	,	4	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
+
+var monthlySales = [	185	,	175	,	234	,	116	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
+var ebayMonthlySales = [	98	,	106	,	144	,	78	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
+var poshMonthlySales = [	41	,	58	,	72	,	25	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
+var mercariMonthlySales = [	21	,	5	,	16	,	13	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
 var fbmpMonthlySales = [	25	,	6	,	2	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
 
-var monthlyRevenue = [	5111.97	,	4594.85	,	6301.81	,	363.25	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
-var ebayMonthlyRevenue = [	2528.97	,	2743.85	,	3518.81	,	185.25	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
-var poshMonthlyRevenue = [	1368	,	1560	,	2297	,	93	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
-var mercariMonthlyRevenue = [	498	,	115	,	401	,	85	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
+var monthlyRevenue = [	5111.97	,	4594.85	,	6276.81	,	2336.22	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
+var ebayMonthlyRevenue = [	2528.97	,	2743.85	,	3493.81	,	1505.22	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
+var poshMonthlyRevenue = [	1368	,	1560	,	2297	,	541	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
+var mercariMonthlyRevenue = [	498	,	115	,	401	,	290	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
 var fbmpMonthlyRevenue = [	717	,	176	,	85	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	];
 
 // WEEKLY NUMBERS
 
-var weekNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+var weekNumber = [	2	,	3	,	4	,	5	,	6	,	7	,	8	,	9	,	10	,	11	,	12	,	13	,	14	,	15	,	16	];
+var ebayWeeklySales = [	26	,	19	,	19	,	21	,	25	,	25	,	25	,	33	,	23	,	30	,	37	,	20	,	54	,	51	,	14	];
+var poshWeeklySales = [	10	,	6	,	7	,	12	,	8	,	7	,	24	,	18	,	15	,	21	,	14	,	17	,	15	,	16	,	4	];
+var mercariWeeklySales = [	5	,	5	,	4	,	6	,	5	,	0	,	0	,	1	,	3	,	3	,	2	,	6	,	6	,	8	,	1	];
+var fbmpWeeklySales = [	9	,	3	,	6	,	3	,	4	,	1	,	2	,	1	,	0	,	0	,	1	,	1	,	0	,	0	,	0	];
 
-var ebayWeeklySales = [26, 19, 19, 21, 25, 25, 25, 33, 23, 31, 37, 20, 53];
-var poshWeeklySales = [10, 6, 7, 12, 8, 7, 24, 18, 15, 21, 14, 17, 15];
-var mercariWeeklySales = [5, 5, 4, 6, 5, 0, 0, 1, 3, 3, 2, 6, 6];
-var fbmpWeeklySales = [9, 3, 6, 3, 4, 1, 2, 1, 0, 0, 1, 1, 0];
-var revenueWeeklySales = [1376.45, 800.98, 1106.02, 1288.73, 1227.76, 752.83, 1292.36, 1414.69, 1155.73, 1914.79, 1532, 920, 1364.54];
+var revenueWeeklySales = [	1376.45	,	800.98	,	1106.02	,	1288.73	,	1227.76	,	752.83	,	1292.36	,	1414.69	,	1155.73	,	1869.79	,	1532	,	920	,	1384.54	,	1634.98	,	337.99	];
 
+var ebayWeeklyRevenue= [	707.45	,	447.98	,	559.02	,	564.73	,	795.76	,	573.83	,	594.36	,	845.69	,	719.73	,	199	,	870	,	299	,	958.54	,	1074.98	,	244.99	];
+var poshWeeklyRevenue = [	301	,	158	,	239	,	481	,	250	,	169	,	638	,	474	,	355	,	0	,	536	,	459	,	311	,	375	,	73	];
+var mercariWeeklyRevenue = [	138	,	95	,	93	,	146	,	121	,	0	,	0	,	20	,	81	,	0	,	86	,	117	,	115	,	185	,	20	];
+var fbmpWeeklyRevenue = [	230	,	100	,	210	,	97	,	61	,	10	,	60	,	75	,	0	,	0	,	40	,	45	,	0	,	0	,	0	];
 
 var ebayColor = ["rgba(0, 99, 145)"];
 var poshColor = ["rgba(246, 149, 156)"];
